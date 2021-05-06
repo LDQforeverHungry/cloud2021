bdac811b7e4afdf87a508f4ab0de29924c36d6d1
ldq <ldq@2021.com>
2021-05-06 22:51:04 +0800

    hot-fix commit2

--- src/main/java/com/autuigu/git.java
+++ src/main/java/com/autuigu/git.java
@@ -11,6 +11,7 @@ public class git {
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("hot-fix git4");
     }
 
 }
