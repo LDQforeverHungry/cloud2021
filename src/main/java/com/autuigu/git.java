e2de7e26dfa1438b7331f137bdb69a26ca2e56f7
ldq <ldq@2021.com>
2021-05-06 19:49:16 +0800

    first commit

---
+++ src/main/java/com/autuigu/git.java
@@ -0,0 +1,13 @@
+package com.autuigu;
+
+/**
+ * @author LDQ
+ * date 2021-05-06
+ */
+public class git {
+
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+}
